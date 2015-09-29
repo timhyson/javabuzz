c8463b9ed84df71402cf67ea00e726b07b8136af
Tim Hyson <tjhyson@gmail.com>
2015-09-29 12:01:32 +0100

    refactoring of Javabuzz function

--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -1,13 +1,17 @@
 var Javabuzz = function() {};
 
+Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return (number % divisor === 0);
+};
+
 Javabuzz.prototype.isDivisibleByThree = function(number) {
-  return (number % 3 === 0);
+  return this._isDivisibleBy(number, 3);
 };
 
 Javabuzz.prototype.isDivisibleByFive = function(number) {
-  return (number % 5 === 0);
+  return this._isDivisibleBy(number, 5);
 };
 
 Javabuzz.prototype.isDivisibleByFifteen = function(number) {
-  return (number % 15 === 0);
+  return this._isDivisibleBy(number, 15);
 };
